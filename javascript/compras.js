b0f3a790686f880ffddb0d4d4e421863a1c4e068
Ezequiel Estabre <e.estabre@outlook.com>
2023-04-03 16:19:36 -0300

    proyecto final

--- javascript/compras.js
+++
@@ -1,5 +0,0 @@
-// const shopContent = document.getElementById
-// const verCarrito = document.getElementById
-// const modalContainer = document.getElementById
-// const showAlert = document.getElementById
-// const cantidadCarrito = document.getElementById
